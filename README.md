4a9d966618b90d8c8a988115d13764033ec5a8b6
Brian Sparker <brainsparker@users.noreply.github.com>
2018-01-26 13:17:26 -0500

    Tweaks (#1)
    Made tweaks as per client's requests.

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-# TheMichaelScott_RedditBot
-A reddit bot that monitors comments on subreddits associated with the NBC series "The Office" and replies to comments that merit the well-known Michael Scott reply "That's what she said!"  
+# rFeedback_RedditBot
+A reddit bot that monitors for new posts on the r/Feedback subreddit and if the OP has not yet contributed to other posts the post will be removed and a DM will be sent to the OP explaining why.
   
-# Future Features/Plans  
-   
- - Providing randomized gif instead of one static gif
- - Adding responses to other Michael Scott-isms
- - Adding responses for other Office staff (Kevin, Dwight, Jim, etc.)
-  
-#  
-  
-If you find this bot entertaining and would like to donate, you can do so using one of the following methods:  
-**Paypal email:** michaelhill24@comcast.net  
-**BTC address:** 1CJa6ptW65qSmSi15jKKgGZNYGd27zCjTU  
-**BCH address:** 1EwJjphxZfzqwfixjNZQL7wLv1Bc36nF9K  
-**ETH address:** 0x44af90711c7F7E9aeCd62468b99d9E34dc10C96D  
-**LTC address:** Lf8HPBNahmygWZJKbasvF2GZ65deFT6eHK  
-**Coinbase email** (To avoid tx fees): mhilluniversal@gmail.com
---
+++ feedbackbot/bot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+from  configparser import SafeConfigParser
+import praw
+import random
+import logging
+import time
+import os
+
+message_subject = 'Your latest post in r/Feedback'
+message_body = ('Your latest post in r/Feedback has been removed because '
+		'according to your comment history you have not yet contributed '
+		'feedback to others. You may post in the subreddit once '
+		'you have participated in the community.')
+
+reddit_bot = praw.Reddit(user_agent=os.environ['user_agent'],
+				      client_id=os.environ['client_id'],
+				      client_secret=os.environ['client_secret'],
+				      username=os.environ['username'],
+				      password=os.environ['password'])
+
+now = int(time.time())
+
+for submission in reddit_bot.subreddit('feedback').new():
+    submission_time = int(submission.created_utc) #or .created
+    if (now - submission_time) > 600:
+        break
+    else:
+        op = reddit_bot.submission.author
+	print('OP is ' + op)
+        noncontributor = True
+        for comment in reddit_bot.redditor(op).comments.new(limit=None):
+            print('comment subreddit : %s'%comment.subreddit
+            if comment.subreddit == 'feedback':
+                noncontributor = False
+                break
+        if noncontributor:
+            print('User hasn\'t contributed. Removing Submission')
+            reddit_bot.submission.SubmissionModeration(submission).remove()
+            print('Notifying User of Submission Removal via DM')
+            reddit_bot.redditor(op).message(message_subject,message_body,from_subreddit='feedback')
